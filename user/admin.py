77a56ea3d3bf230b34404b68bbceeef00223a749
serhii <ockar2018@gmail.com>
2024-10-04 18:47:20 +0300

    fix

--- user/admin.py
+++ user/admin.py
@@ -25,4 +25,3 @@ class UserAdmin(UserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
-
